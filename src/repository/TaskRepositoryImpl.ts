c7374846bf0de5170d6ebdbe910fa153a1070621
tadashi-aikawa <syou.maman@gmail.com>
2023-01-26 14:07:27 +0900

    タスクファイルと休日ファイルの空行やコメント行を無視する

--- src/repository/TaskRepositoryImpl.ts
+++ src/repository/TaskRepositoryImpl.ts
@@ -14,14 +14,17 @@ export class TaskRepositoryImpl implements TaskRepository {
   loadRepetitionTasks(): AsyncResult<RepetitionTask[], BaseError> {
     return fromPromise(
       this.appHelper.loadFile(this.repetitionTasksFilePath).then((tasksStr) =>
-        tasksStr.split("\n").map((line) => {
-          const [name, repetition, baseDate] = line.split(",");
-          return RepetitionTask.of({
-            name,
-            repetition: Repetition.from(repetition),
-            baseDate: baseDate ? DateTime.of(baseDate) : undefined,
-          });
-        })
+        tasksStr
+          .split("\n")
+          .filter((line) => !line.startsWith("//") && line.trim() !== "")
+          .map((line) => {
+            const [name, repetition, baseDate] = line.split(",");
+            return RepetitionTask.of({
+              name,
+              repetition: Repetition.from(repetition),
+              baseDate: baseDate ? DateTime.of(baseDate) : undefined,
+            });
+          })
       )
     );
   }
@@ -29,9 +32,12 @@ export class TaskRepositoryImpl implements TaskRepository {
   loadHolidays(): AsyncResult<DateTime[], BaseError> {
     return fromPromise(
       this.holidaysFilePath
-        ? this.appHelper
-            .loadFile(this.holidaysFilePath)
-            .then((holidaysStr) => holidaysStr.split("\n").map(DateTime.of))
+        ? this.appHelper.loadFile(this.holidaysFilePath).then((holidaysStr) =>
+            holidaysStr
+              .split("\n")
+              .filter((line) => !line.startsWith("//") && line.trim() !== "")
+              .map(DateTime.of)
+          )
         : Promise.resolve([])
     );
   }
