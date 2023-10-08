20526ac0e90dc35f416a8067808f8dc98d82392a
tadashi-aikawa <syou.maman@gmail.com>
2023-10-08 13:18:55 +0900

    タブや全角スペースのインデントでInsert current timeの挿入位置がおかしくなる不具合修正

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -3,7 +3,7 @@ export function parseMarkdownList(text: string): {
   content: string;
 } {
   const { groups } = Array.from(
-    text.matchAll(/^(?<prefix> *([-*] (\[.] |)|))(?<content>.*)$/g)
+    text.matchAll(/^(?<prefix>[ \t\s]*([-*] (\[.] |)|))(?<content>.*)$/g)
   ).at(0) as any;
 
   return { prefix: groups.prefix, content: groups.content };
