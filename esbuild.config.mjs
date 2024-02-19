0c7fe973b72c0fe57635c574d7385749b8b94afc
tadashi-aikawa <syou.maman@gmail.com>
2024-02-19 22:34:03 +0900

    フォーマットや実行権限付与

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -13,53 +13,44 @@ import sveltePreprocess from "svelte-preprocess";
 const prod = process.argv[2] === "production";
 
 const context = await esbuild.context({
-    plugins: [
-      esbuildSvelte({
-        compilerOptions: { css: true },
-        preprocess: sveltePreprocess(),
-      }),
-    ],
-    banner: {
-      js: banner,
-    },
-    entryPoints: ["src/main.ts"],
-    bundle: true,
-    external: [
-      "obsidian",
-      "electron",
-      "@codemirror/autocomplete",
-      "@codemirror/collab",
-      "@codemirror/commands",
-      "@codemirror/language",
-      "@codemirror/lint",
-      "@codemirror/search",
-      "@codemirror/state",
-      "@codemirror/view",
-      "@lezer/common",
-      "@lezer/highlight",
-      "@lezer/lr",
-      ...builtins,
-    ],
-    format: "cjs",
-    target: "es2018",
-    logLevel: "info",
-    sourcemap: prod ? false : "inline",
-    treeShaking: true,
-    outfile: "main.js",
-  })
+  plugins: [
+    esbuildSvelte({
+      compilerOptions: { css: true },
+      preprocess: sveltePreprocess(),
+    }),
+  ],
+  banner: {
+    js: banner,
+  },
+  entryPoints: ["src/main.ts"],
+  bundle: true,
+  external: [
+    "obsidian",
+    "electron",
+    "@codemirror/autocomplete",
+    "@codemirror/collab",
+    "@codemirror/commands",
+    "@codemirror/language",
+    "@codemirror/lint",
+    "@codemirror/search",
+    "@codemirror/state",
+    "@codemirror/view",
+    "@lezer/common",
+    "@lezer/highlight",
+    "@lezer/lr",
+    ...builtins,
+  ],
+  format: "cjs",
+  target: "es2018",
+  logLevel: "info",
+  sourcemap: prod ? false : "inline",
+  treeShaking: true,
+  outfile: "main.js",
+});
 
 if (prod) {
-	await context.rebuild();
- 	process.exit(0);
- } else {
-   await context.watch();
- }
-
-
-
-
-
-
-
-
-
+  await context.rebuild();
+  process.exit(0);
+} else {
+  await context.watch();
+}
